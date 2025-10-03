9e00b49d0ccf107e4457a2c24c532d4649880000
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-10-03 14:43:49 -0300

    chore: spring boot project setup

---
+++ src/main/java/com/paula/click2buy/Click2buyApplication.java
@@ -0,0 +1,13 @@
+package com.paula.click2buy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Click2buyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Click2buyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paula/click2buy/Click2buyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paula.click2buy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Click2buyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
