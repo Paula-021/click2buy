8b147dac68ab088c6599abd7deb10b527c92c8d8
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-10-20 11:22:14 -0300

    feat: add UserRepository class

---
+++ src/main/java/com/paula/click2buy/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.paula.click2buy.repositories;
+
+import com.paula.click2buy.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
