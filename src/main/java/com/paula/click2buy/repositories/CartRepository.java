3b12d6f0e5edd3063e7591a652cbd18f55e0ce36
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-11-13 10:23:43 -0300

    feat: add CartRepository interface for cart data management

---
+++ src/main/java/com/paula/click2buy/repositories/CartRepository.java
@@ -0,0 +1,9 @@
+package com.paula.click2buy.repositories;
+
+import com.paula.click2buy.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
