e79ee3654267b1091370bdcac2b7160084dc41c2
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-12-05 18:40:07 -0300

    feat: add exception handler for StockQuantityNotFoundException in ProductRestExceptionHandler

--- src/main/java/com/paula/click2buy/endpoints/ProductRestExceptionHandler.java
+++ src/main/java/com/paula/click2buy/endpoints/ProductRestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.paula.click2buy.endpoints;
 
 
 import com.paula.click2buy.exceptions.ProductNotFoundException;
+import com.paula.click2buy.exceptions.StockQuantityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,12 @@ public class ProductRestExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<?> handlerException(ProductNotFoundException productNotFoundException) {
         ErrorResponse errorResponse = new ErrorResponse(404, productNotFoundException.getMessage(),System.currentTimeMillis());
+        return ResponseEntity.status(404).body(errorResponse);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handlerException(StockQuantityNotFoundException stockQuantityNotFoundException) {
+        ErrorResponse errorResponse = new ErrorResponse(404, stockQuantityNotFoundException.getMessage(),System.currentTimeMillis());
         return ResponseEntity.status(404).body(errorResponse);
     }
 }
