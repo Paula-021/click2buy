ca9f44f3ab9625d29025d5a8568d6097fb0cfbea
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-12-05 18:29:52 -0300

    feat: update CartService and CartServiceImpl to manage items in the cart

--- src/main/java/com/paula/click2buy/services/CartService.java
+++ src/main/java/com/paula/click2buy/services/CartService.java
@@ -2,13 +2,15 @@ package com.paula.click2buy.services;
 
 import com.paula.click2buy.domain.Cart;
 import com.paula.click2buy.endpoints.dtos.CartRequestDTO;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public interface CartService {
-    Cart addCart(CartRequestDTO cartRequestDTO);
+    
+    Cart addCart();
 
     void updateCart(Cart cart);
 
@@ -17,4 +19,10 @@ public interface CartService {
     Cart getCartById(Long id);
 
     List<Cart> getAllCarts();
+
+    Cart addItemsToCart(Long cartId, @Valid CartRequestDTO cartRequestDTO);
+
+    Cart removeOneItemFromTheCart(Long cartId, Long itemId);
+
+    Cart removeItemFromCart(Long cartId, Long itemId);
 }
