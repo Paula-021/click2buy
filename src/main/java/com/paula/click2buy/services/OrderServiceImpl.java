add86738a82249f014efc088a7302ea2c0092df1
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-12-15 18:17:06 -0300

    feat: implement order processing logic including total price calculation and order status management

--- src/main/java/com/paula/click2buy/services/OrderServiceImpl.java
+++ src/main/java/com/paula/click2buy/services/OrderServiceImpl.java
@@ -1,10 +1,12 @@
 package com.paula.click2buy.services;
 
 import com.paula.click2buy.domain.Order;
+import com.paula.click2buy.domain.OrderStatus;
 import com.paula.click2buy.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -15,6 +17,28 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void addOrder(Order order) {
+
+        //calcular o preço total do pedido
+        double totalPrice = order.getCart().getListItemCart().stream()
+                .mapToDouble(item -> item.getProduct().getPrice() * item.getQuantity())
+                .sum();
+
+        //expectedDate
+        //fazer a consulta na API de frete para calcular a data de entrega
+
+        //orderDate
+        order.setOrderDate(LocalDate.now());
+
+        //orderStatus
+        if(order.isPaid()){
+            order.setOrderStatus(OrderStatus.PENDING_SHIPMENT);
+        } else {
+            order.setOrderStatus(OrderStatus.AWAITING_PAYMENT);
+        }
+
+        //trackingNumber - gerar um código aleatório
+
+        //paid -> receber da API de pagamento
 
         orderRepository.save(order);
 
