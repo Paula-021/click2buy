bc85fe13fd14ff8103056a64138a417419433dc2
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2026-04-28 16:38:02 -0300

    feat: add order management endpoints for marking as paid, updating status, address, and cancellation

--- src/main/java/com/paula/click2buy/endpoints/OrderController.java
+++ src/main/java/com/paula/click2buy/endpoints/OrderController.java
@@ -8,6 +8,7 @@ import com.paula.click2buy.endpoints.dtos.OrderResponseDTO;
 import com.paula.click2buy.services.CartService;
 import com.paula.click2buy.services.OrderService;
 import com.paula.click2buy.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,10 +29,11 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<?> addOrder(@RequestBody OrderRequestDTO orderRequestDTO) {
+    public ResponseEntity<?> addOrder(@Valid @RequestBody OrderRequestDTO orderRequestDTO) {
         // Lógica para criar o pedido com base no orderRequestDTO
         Cart cart = cartService.getCartById(orderRequestDTO.getIdCart());
         User user = userService.getUserById(orderRequestDTO.getIdUser());
+
 
         Order order = orderRequestDTO.toEntity(cart, user);
 
@@ -49,4 +51,32 @@ public class OrderController {
 
         return ResponseEntity.ok().body(ordersResponseDTO);
     }
+
+    @PutMapping("/{id}/paid")
+    public ResponseEntity<?> markOrderAsPaid(@PathVariable Long id) {
+        orderService.markOrderAsPaid(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}/status") //prosseguir o status do pedido (1. PENDING_SHIPPING, 2. SHIPPED, 3. DELIVERED)
+        public ResponseEntity<?> updateOrderStatus(@PathVariable Long id) {
+            orderService.updateOrderStatus(id);
+            return ResponseEntity.ok().build();
+        }
+
+
+    @PutMapping("/{id}/address") //atualizar o endereço do pedido
+    public ResponseEntity<?> updateOrderAddress(@PathVariable Long id, @RequestBody Long idAddress) {
+        orderService.updateOrderAddress(id, idAddress);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}/cancel") //cancelar o pedido
+     public ResponseEntity<?> updateOrderStatusCancel(@PathVariable Long id) {
+        orderService.updateOrderStatusCancel(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+
 }
