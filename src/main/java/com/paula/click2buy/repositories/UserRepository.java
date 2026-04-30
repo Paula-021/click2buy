0b55ceab6652211f30aaf79168d4563effe4253d
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2026-04-30 20:18:07 -0300

    feat: add method to find user by email in UserRepository

--- src/main/java/com/paula/click2buy/repositories/UserRepository.java
+++ src/main/java/com/paula/click2buy/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import com.paula.click2buy.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
